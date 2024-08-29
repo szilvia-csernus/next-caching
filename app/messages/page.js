1767526eebc96ce12a9bfb60d3d72ab0c5cb88b0
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-29 12:32:16 +0100

    demonstrate data caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,10 +1,25 @@
 import Messages from '@/components/messages';
 
+import { unstable_noStore } from 'next/cache'; // not yet ready for production
+
+// This is a reserved constant keyword in Next.js, it will be used for the fetch requests.
+// With this, we don't need to set this value in every fetch request.
+// export const revalidate = 5; // seconds
+
+// This is another reserved constant keyword in Next.js, used for setting the cache policy.
+// export const dynamic = 'auto'; // default
+export const dynamic = 'force-dynamic'; // same as setting cache: 'no-store', i.e. don't cache this request
+// export const dynamic = 'force-static'; // it will cache the request and serve it from the cache only, never revalidate
+// All these constants are used throughout the file.
+
 export default async function MessagesPage() {
+  unstable_noStore(); // component-specific setting, behaviour as dynamic = 'force-dynamic'
   const response = await fetch('http://localhost:8080/messages', {
-    // headers: {
-    //   'X-ID': 'page',
+    // These are request-specific settings, they will override the component-specific settings
+    // next: {
+    //   revalidate: 5 // seconds
     // },
+    // cache: 'no-store' // this is telling nextjs not to cache this request
   });
   const messages = await response.json();
 
